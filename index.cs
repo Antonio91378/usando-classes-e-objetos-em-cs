faa700b12c654c7becc66e51deaca0435c356e28
Antonio <antoniovictor200@gmail.com>
2022-05-16 11:28:19 -0300

    first

--- index.cs
+++ index.cs
@@ -2,19 +2,30 @@ using System;
 
 public class Jogador
 {
-    public int energia = 100;
+    public int energia;
 
-    public bool vivo = true;
+    public bool vivo;
+
+    public string nome;
+
+    public Jogador(string n)
+    {
+        energia = 100;
+        vivo = true;
+        nome = n;
+    }
 }
 
 class Principal
 {
     static void Main()
     {
-        Jogador j1 = new Jogador();
-        Jogador j2 = new Jogador();
-        Jogador j3 = new Jogador();
+        Jogador j1 = new Jogador("Carlos");
+        Jogador j2 = new Jogador("Antonio");
+        Jogador j3 = new Jogador("Lucas");
 
-        Console.WriteLine("A energia do jogador 1 é : {0}", j1.energia);
+        Console.WriteLine("O nome do jogador 1 é : {0}", j1.nome);
+        Console.WriteLine("O nome do jogador 2 é : {0}", j2.nome);
+        Console.WriteLine("O nome do jogador 3 é : {0}", j3.nome);
     }
 }
